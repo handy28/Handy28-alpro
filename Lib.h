9fdf2ff72e3d118d558e0c5e48d0b39686212b67
handy28 <handyher28@gmail.com>
2017-02-18 06:36:51 +0700

    Add files via upload

---
+++ Lib.h
@@ -0,0 +1,6 @@
+//I.S Membuat Aplikasi dengan 3 File
+//F.S Menampilkan Hasil dari Fungsi Call by Value
+
+//Prototype di Program13.cpp
+
+void swap (int x, int y); //Memangil fungsi pada file swap.cpp
