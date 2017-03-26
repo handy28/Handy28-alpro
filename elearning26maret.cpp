cf6f4a8fa41d6be69ce9fd601d74b00a24c44742
handy28 <handyher28@gmail.com>
2017-03-26 12:03:06 +0700

    Add files via upload

---
+++ elearning26maret.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+// {i.s} Inisialisasi Fungsi
+// {f.s} Pemanggilan Fungsi
+using namespace std;
+
+// Inisialisasi Fungsi
+ void contoh() {
+  cout << "Latihan modular " << endl;
+
+}
+
+//Inisialisai Fungsi
+ void Nama(string nama) {
+  cout << "Haii " << nama << endl;
+}
+
+int main(){
+
+  contoh(); // pemanggilan fungsi
+
+
+  Nama("Handi Hermawan"); // pemanggilan fungsi
+
+  cout << "Selamat Mengerjakan" << endl;
+
+  return 0;
+}
